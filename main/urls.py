2f1369549e39f87f43ea4f60d1e1ed77fc7d2dd3
Jabborova Ziyodaxon <ziyodaxon1309@gmail.com>
2025-12-07 11:18:38 +0500

    Connect all pages to their static files

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home_view,product_view,contact_view
+from .views import home_view,property_view,contact_view,property_details_view
 
 
 urlpatterns = [
     path("",home_view,name="home"),
-    path("/product",product_view,name="product"),
+    path("/product",property_view,name="property"),
     path("/contact",contact_view,name="contact"),
+    path("/property-details/<int:pk>/",property_details_view,name="contact"),
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
+# from .models import 
 
 def home_view(request):
     return render(request,"main/index.html")
 
-def product_view(request):
+def property_view(request):
     return render(request,"main/properties.html")
+
+def property_details_view(request,pk):
+    # house=get_object_or_404(House)
+    return render(request,"main/property-details.html")
 
 def contact_view(request):
     return render(request,"main/contact.html")
